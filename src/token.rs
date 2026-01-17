e9d9398b896316955fd1fb33f5de383bbc196a02
Samii Shabuse <sshabuse@gmail.com>
2026-01-17 15:31:37 -0500

    feature(tokens): create token.rs to hold token values

---
+++ src/token.rs
@@ -0,0 +1,56 @@
+pub enum Token {
+    // Fixed Punctuations
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    CARET,
+    AMPERSAND,
+    ATSIGN,
+    NOT,
+    DOT,
+    COLON,
+    COMMA,
+
+    // Keywords
+    THIS,
+    IF,
+    IFONLY,
+    WHILE,
+    RETURN,
+    PRINT,
+    EOF,
+
+    // Tokens with data
+    OPERATOR,
+    NUMBER,
+    IDENTIFIER,
+}
+
+imp Token {
+    pub fn token_type(&self) -> TokenType {
+        match self {
+            Token::LEFT_PAREN => TokenType::LEFT_PAREN,
+            Token::RIGHT_PAREN => TokenType::RIGHT_PAREN,
+            Token::LEFT_BRACE => TokenType::LEFT_BRACE,
+            Token::RIGHT_BRACE => TokenType::RIGHT_BRACE,
+            Token::CARET => TokenType::CARET,
+            Token::AMPERSAND => TokenType::AMPERSAND,
+            Token::ATSIGN => TokenType::ATSIGN,
+            Token::NOT => TokenType::NOT,
+            Token::DOT => TokenType::DOT,
+            Token::COLON => TokenType::COLON,
+            Token::COMMA => TokenType::COMMA,
+            Token::THIS => TokenType::THIS,
+            Token::IF => TokenType::IF,
+            Token::IFONLY => TokenType::IFONLY,
+            Token::WHILE => TokenType::WHILE,
+            Token::RETURN => TokenType::RETURN,
+            Token::PRINT => TokenType::PRINT,
+            Token::EOF => TokenType::EOF,
+            Token::OPERATOR(_) => TokenType::OPERATOR,
+            Token::NUMBER(_) => TokenType::NUMBER,
+            Token::IDENTIFIER(_) => TokenType::IDENTIFIER,
+        }
+    }
+}
