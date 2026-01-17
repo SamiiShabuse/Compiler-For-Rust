738f3cbb6c222ed65ee9e72d0508fa54fdf0ec1a
Samii Shabuse <sshabuse@gmail.com>
2026-01-17 15:24:50 -0500

    fature(main): create input checker for either tokenizer or parseExpr

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,42 @@ fn main() {
         eprintln!("{}", err);
         std::process::exit(1);
     }
+
+    // going to copy general flow from professor's Java code
+    // TODO: Refactor later
+    let sub = &args[1];
+
+    let mut input = String::new();
+    for (i, part) in args[2..].iter().enumerate() {
+        input.push_str(part);
+        if i != args.len() - 3 {
+            input.push(' ');
+        }
+    }
+
+    // create this
+    let tok = Tokenizer::new(&input);
+
+    match sub.as_str() {
+        "tokenize" => {
+            let mut t= tok;
+            loop {
+                let p = t.peek();
+                if p.token_type == TokenType::EOF {
+                    break;
+                }
+                println!("{:?}", t.next());
+            }
+        }
+        "parseExpr" => {
+            let mut p= Parser::new(tok);
+            let expr = p.parse_expr();
+            println!("{:?}", expr);
+        }
+        _ => {
+            eprintln!("Unknown subcommand: {}", sub);
+            std::process::exit(1);
+        }
+    }
 }
 
